dc4959807622b18c6502f3a5e9a819517042a504
Midnight-Knight <LTupov2003@yandex.ru>
2022-09-19 14:09:03 +0300

    19.09.22

--- src/ru/mirea/prk1/main.java
+++ src/ru/mirea/prk1/main.java
@@ -1,8 +1,23 @@
 package ru.mirea.prk1;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args)
     {
-        
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        float[] A= new float[N];
+        float Sum = 0;
+        for (int i = 0; i < N; ++i)
+        {
+            A[i] = scanner.nextFloat();
+            Sum += A[i];
+        }
+        float Aver = Sum / N;
+        for (int i = 0; i < N; ++i)
+        {
+            System.out.printf("%s ",A[i]);
+        }
+        System.out.printf("%nСумма: %s%nСреднее арифметическое: %s",Sum, Aver);
     }
 }
