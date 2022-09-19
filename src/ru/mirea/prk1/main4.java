4ae29a94552f6eff4ebfa4088ce64a00f0f7092e
Midnight-Knight <LTupov2003@yandex.ru>
2022-09-19 14:56:02 +0300

    19.09.22

---
+++ src/ru/mirea/prk1/main4.java
@@ -0,0 +1,11 @@
+package ru.mirea.prk1;
+
+public class main4 {
+    public static void main(String[] args)
+    {
+        for(float i = 1; i <= 10; ++i)
+        {
+            System.out.printf("1.0/%s = %s%n",i,(1.0/i));
+        }
+    }
+}
