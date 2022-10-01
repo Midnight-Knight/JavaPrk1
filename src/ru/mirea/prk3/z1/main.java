3f580aed49363f792a49f6257bfded9ca5675a0b
Midnight-Knight <LTupov2003@yandex.ru>
2022-10-01 18:17:39 +0300

    01 10 22 2

--- src/ru/mirea/prk3/z1/main.java
+++ src/ru/mirea/prk3/z1/main.java
@@ -4,6 +4,22 @@ public class main {
     public static void main(String[] args)
     {
         Double A = Double.valueOf(5);
-        System.out.println(A);
+        System.out.printf("Double: %s%n",A);
+        String B = "4";
+        A = Double.parseDouble(B);
+        System.out.printf("Double: %s%n",A);
+        int C1 = A.intValue();
+        System.out.printf("int: %s%n",C1);
+        byte C2 = A.byteValue();
+        System.out.printf("byte: %s%n",C2);
+        short C3 = A.shortValue();
+        System.out.printf("short: %s%n",C3);
+        long C4 = A.longValue();
+        System.out.printf("long: %s%n",C4);
+        float C5 = A.floatValue();
+        System.out.printf("float: %s%n",C5);
+        double C6 = A.doubleValue();
+        System.out.printf("double: %s%n",C6);
+        System.out.printf("Double: %s%n",A);
     }
 }
