58051a64cd2cbbcf3d3455b96dc6edbe8e505608
Midnight-Knight <LTupov2003@yandex.ru>
2022-10-01 17:29:14 +0300

    01 10 22

--- src/ru/mirea/prk2/z9/Poker.java
+++ src/ru/mirea/prk2/z9/Poker.java
@@ -1,18 +1,29 @@
 package ru.mirea.prk2.z9;
 import java.util.Scanner;
 
+
 public class Poker {
     public static void main(String[] args)
     {
+        String[] suits = {"Пик","Бубен","Червь","Треф"};
+        String[] rank = {"2","3","4","5","6","7","8","9","10","Валет","Королева","Король","Туз"};
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        String c = "";
+        int x;
         for (int i = 0; i < n; ++i)
         {
+            c = "";
             for (int j = 0; j < 5; ++j)
             {
-                System.out.print(j+1);
+                x = (int)(Math.random()*(12-0));
+                c+=rank[x];
+                c+=" ";
+                x = (int)(Math.random()*(3-0));
+                c+=suits[x];
+                c+=" | ";
             }
-            System.out.println();
+            System.out.println(c);
         }
     }
 }
---
+++ src/ru/mirea/prk3/z1/main.java
@@ -0,0 +1,9 @@
+package ru.mirea.prk3.z1;
+
+public class main {
+    public static void main(String[] args)
+    {
+        Double A = Double.valueOf(5);
+        System.out.println(A);
+    }
+}
